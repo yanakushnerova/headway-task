9315567e8c25fba2b99367830473ef24ca697d7e
yanakushnerova <yanakushneriova@gmail.com>
2026-03-16 12:53:30 +0200

    Setup eslint, prettier, and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ['@typescript-eslint'],
+      extends: ['airbnb-typescript', 'prettier'],
+    },
+  ],
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main} />
     </div>
   );
 }
